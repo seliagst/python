f750b93d9f6921752e3ffa43abcac0e038facdb6
seliagst <111722599+seliagst@users.noreply.github.com>
2022-10-20 10:16:48 +0700

    Add files via upload

---
+++ latihan03.py
@@ -0,0 +1,47 @@
+# import library
+import time
+import datetime
+
+# input username
+name = input("Hello... my name is Mr. Python, what is your name? ")
+
+# output username
+print("Oh.. your name is ", name, ", such a nice name.")
+
+
+# pause 3 seconds 
+time.sleep(3)
+
+# input year or birth
+birthYear = int(input("Well.. " + name + ", when where you born? "))
+
+# pause 3 seconds
+time.sleep(3)
+
+# calculate age 
+timeNow = datetime.datetime.now()
+age = timeNow.year - birthYear
+
+# tampilkan usia
+print("Hmmm...", name,"so you are", age," years old, aren’t you?")
+
+# pause 3 seconds
+time.sleep(3)
+
+# show the message based on age
+if (age > 40):
+    print("You are too old ?")
+    print("Take care of your health, OK!!")
+elif (age > 17):
+    print("You are still young")
+    print("Don't waste your life with unimportant activities!!")
+else:
+    print("You are still a child?")
+    print("Be a good child!!")
+
+# pause 3 seconds
+time.sleep(3)
+
+# say goodbye
+print("OK.. see you later", name, ".. nice to meet you")
+
