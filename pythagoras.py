6f314cc3c6bc1f4e81fbc2881b61a2ca71c59da5
seliagst <111722599+seliagst@users.noreply.github.com>
2022-11-07 17:00:49 +0700

    Delete pythagoras.py

--- pythagoras.py
+++
@@ -1,8 +0,0 @@
-sisi_depan = int(input("Masukkan sisi depan :"))
-sisi_tegak = int(input("Masukkan sisi tegak :"))
-sisi_miring = int(input("Masukkan sisi miring :"))
-
-if sisi_miring == (sisi_depan ** 2 + sisi_tegak ** 2)**(0.5) :
-    print("->Benar")
-else :
-    print("->Salah")
