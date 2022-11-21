d87147b42865d15a0ec1a76d71d12766c2916c3c
seliagst <111722599+seliagst@users.noreply.github.com>
2022-11-21 08:47:19 +0700

    Delete pythagoras.py

--- pythagoras.py
+++
@@ -1,9 +0,0 @@
-def Pythagoras(a, b, c):
-    if c**2 == a**2 + b**2:
-        print("->Benar")
-    else :
-        print("->Salah")
-x = int(input("Jumlah Percobaan:"))        
-for i in range(x):
-    Pythagoras(int(input("Nilai a:")), int(input("Nilai b:")), int(input("Nilai c:")))
-
