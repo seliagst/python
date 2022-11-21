6ae2e4c710992f0ac521ed6a74bd9526b837c0fd
seliagst <111722599+seliagst@users.noreply.github.com>
2022-11-21 09:30:56 +0700

    Delete STAR2.py

--- STAR2.py
+++
@@ -1,8 +0,0 @@
-def starFormation (n) :
-    for i in range(n) :
-        for j in range(i, n) :
-            print("*", end=" ")
-        print()
-starFormation(int(input("Nilai n:")))
-        
- 
